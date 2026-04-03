e3d9b2927fc5836c956c0cc106774364ae5ebaa3
note.miura <note.miura@estuonoiMac.local>
2026-04-03 17:31:00 +0900

    パスの修正で真っ白画面を直す

--- vite.config.js
+++ vite.config.js
@@ -5,6 +5,5 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
   // GitHub Pagesで公開するための設定
-  // 実際のリポジトリ名がわからないため仮置きしています。「YOUR_REPO_NAME」を書き換えてください。
-  base: '/YOUR_REPO_NAME/',
+  base: '/chemistry-quiz/',
 })
